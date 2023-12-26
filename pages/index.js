84d12fee17cde47018542c362c47378b665092a2
MateusProDev <mateusferreiraprodev@gmail.com>
2023-12-25 23:42:02 -0300

    Novas paginas js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,15 @@
-function Home () {
-    return <div>Home </div>
+// pages/index.js
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Next.js!</h1>
+      <Link href="/sobre">
+        Acesse a pagina Home
+      </Link>
+    </div>
+  )
 }
-export default Home
+
+export default Home;
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+function Sobre() {
+    return (
+      <div>
+        <h1>Bem-vindo ao Next.js!</h1>
+        <Link href="/">
+            Acesse a pagina sobre
+        </Link>
+      </div>
+    )
+  }
+  
+  export default Sobre;
